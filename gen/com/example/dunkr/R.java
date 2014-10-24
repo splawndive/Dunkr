ece1dc512a6602054108e3d902a94e1e94ec1048
splawndive <stperry2@gmail.com>
2014-10-24 03:47:47 -0700

    Phase 1
    Added a countdown button, method to get basic info

--- gen/com/example/dunkr/R.java
+++ gen/com/example/dunkr/R.java
@@ -1649,13 +1649,14 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f090030;
         public static final int action_mode_bar_stub=0x7f09002f;
         public static final int action_mode_close_button=0x7f090022;
-        public static final int action_settings=0x7f090041;
+        public static final int action_settings=0x7f090043;
         public static final int activity_chooser_view_content=0x7f090023;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
         public static final int bottom=0x7f09001e;
         public static final int checkbox=0x7f09002b;
         public static final int collapseActionView=0x7f090015;
+        public static final int countdown_button=0x7f090042;
         public static final int decor_content_parent=0x7f090031;
         public static final int default_activity_button=0x7f090026;
         public static final int dialog=0x7f090019;
@@ -1672,6 +1673,7 @@ containing a value of this type.
         public static final int image=0x7f090025;
         public static final int listMode=0x7f09000a;
         public static final int list_item=0x7f090027;
+        public static final int maintext=0x7f090041;
         public static final int middle=0x7f09001d;
         public static final int never=0x7f090017;
         public static final int none=0x7f09000f;
---
+++ src/com/dunkr/MainActivity.java
@@ -0,0 +1,126 @@
+package com.dunkr;
+
+import com.example.dunkr.R;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity implements SensorEventListener, View.OnClickListener {
+
+	private TextView tv;
+	private Button count_btn;
+	
+	private CountDownTimer timer;
+	private int countdown = 5000;
+	private final int interval = 1000;
+	
+	private long lastUpdate;
+	private float prev_x, prev_y, prev_z;
+	private final float alpha = (float) 0.8;
+	
+	Sensor senAccelerometer;
+	SensorManager senSensorManager;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		tv = (TextView) findViewById(R.id.maintext);
+		count_btn = (Button) findViewById(R.id.countdown_button);
+		this.timer = new CountDownTimer(this.countdown,this.interval){
+			
+			@Override
+			public void onTick(long mills_left){
+				count_btn.setText(Long.toString(mills_left/1000));
+			}
+			
+			@Override
+			public void onFinish(){
+				count_btn.setText("Reset Countdown!");
+			}
+			
+		};
+		
+		
+		tv.setText("0,0,0");
+		count_btn.setText("Begin CountDown!");
+		count_btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				timer.start();
+			}	
+		});
+	
+		senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+	    senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	    senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent sensorEvent) {
+		Sensor sensor = sensorEvent.sensor;
+		float[] gravity = new float[3];
+		float[] linear_acceleration = new float[3];
+		
+		if(sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+			float x = sensorEvent.values[0];
+			float y = sensorEvent.values[1];
+			float z = sensorEvent.values[2];
+			
+			long currTime = System.currentTimeMillis();
+			
+			gravity[0] = alpha * gravity[0] + (1 - alpha) * x;
+	        gravity[1] = alpha * gravity[1] + (1 - alpha) * y;
+	        gravity[2] = alpha * gravity[2] + (1 - alpha) * z;
+	
+	        linear_acceleration[0] = x - gravity[0];
+	        linear_acceleration[1] = y - gravity[1];
+	        linear_acceleration[2] = z - gravity[2];
+		}
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+}
