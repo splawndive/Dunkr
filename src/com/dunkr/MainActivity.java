32a33f9c9fc5eb65acc6e682002707c6d72c526f
splawndive <stperry2@gmail.com>
2014-10-25 00:38:32 -0700

    Basic Functionality
    Gets maximum linear acceleration on an arbitrary acces

--- src/com/dunkr/MainActivity.java
+++ src/com/dunkr/MainActivity.java
@@ -22,13 +22,18 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 	private TextView tv;
 	private Button count_btn;
 	
+	//vars used by the countdown timer
 	private CountDownTimer timer;
 	private int countdown = 5000;
 	private final int interval = 1000;
+	private boolean isCounting = false;
+	private float max_accel;
 	
+	
+	//vars used by the accelerometer
 	private long lastUpdate;
-	private float prev_x, prev_y, prev_z;
 	private final float alpha = (float) 0.8;
+	private float prev_x, prev_y, prev_z, max_accelx, max_accely, max_accelz = (float) 0.0;
 	
 	Sensor senAccelerometer;
 	SensorManager senSensorManager;
@@ -50,7 +55,16 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 			
 			@Override
 			public void onFinish(){
+				
 				count_btn.setText("Reset Countdown!");
+				isCounting = false;
+				
+				if(max_accelx >= max_accely && max_accelx >= max_accelz) max_accel = max_accelx;
+				if(max_accely >= max_accelx && max_accely >= max_accelz) max_accel = max_accely;
+				if(max_accelz >= max_accelx && max_accelz >= max_accely) max_accel = max_accelz;
+				
+				tv.setText(Float.toString(max_accel));
+				
 			}
 			
 		};
@@ -61,14 +75,18 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 		count_btn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				isCounting = true;
 				timer.start();
-			}	
+			}
 		});
 	
 		senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 	    senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 	    senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
-	}
+	    
+	} //end of oncreate method
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -91,24 +109,31 @@ public class MainActivity extends ActionBarActivity implements SensorEventListen
 
 	@Override
 	public void onSensorChanged(SensorEvent sensorEvent) {
-		Sensor sensor = sensorEvent.sensor;
-		float[] gravity = new float[3];
-		float[] linear_acceleration = new float[3];
 		
-		if(sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-			float x = sensorEvent.values[0];
-			float y = sensorEvent.values[1];
-			float z = sensorEvent.values[2];
+		if(isCounting){
+			Sensor sensor = sensorEvent.sensor;
+			float[] gravity = new float[3];
+			float[] linear_acceleration = new float[3];
 			
-			long currTime = System.currentTimeMillis();
-			
-			gravity[0] = alpha * gravity[0] + (1 - alpha) * x;
-	        gravity[1] = alpha * gravity[1] + (1 - alpha) * y;
-	        gravity[2] = alpha * gravity[2] + (1 - alpha) * z;
-	
-	        linear_acceleration[0] = x - gravity[0];
-	        linear_acceleration[1] = y - gravity[1];
-	        linear_acceleration[2] = z - gravity[2];
+			if(sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+				float x = sensorEvent.values[0];
+				float y = sensorEvent.values[1];
+				float z = sensorEvent.values[2];
+				
+				long currTime = System.currentTimeMillis();
+				
+				gravity[0] = alpha * gravity[0] + (1 - alpha) * x;
+		        gravity[1] = alpha * gravity[1] + (1 - alpha) * y;
+		        gravity[2] = alpha * gravity[2] + (1 - alpha) * z;
+		
+		        linear_acceleration[0] = x - gravity[0];
+		        linear_acceleration[1] = y - gravity[1];
+		        linear_acceleration[2] = z - gravity[2];
+		        
+		        if(linear_acceleration[0] > max_accelx) max_accelx = linear_acceleration[0];
+		        if(linear_acceleration[1] > max_accely) max_accely = linear_acceleration[1];
+		        if(linear_acceleration[2] > max_accelz) max_accelz = linear_acceleration[2];
+			}
 		}
 	}
 
